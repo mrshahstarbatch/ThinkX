cb9a357b3130e53b1801c871c10fe4b647f8e845
mrshahstarbatch <mrshahstarbatch@gmail.com>
2025-06-20 16:38:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 function scrollToPartners() {
   document.getElementById('partners').scrollIntoView({ behavior: 'smooth' });
 }
+tsParticles.load("tsparticles", {
+  fullScreen: { enable: true, zIndex: -1 },
+  particles: {
+    number: { value: 80 },
+    color: { value: "#00ffcc" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    move: { enable: true, speed: 1 }
+  },
+  interactivity: {
+    events: {
+      onHover: { enable: true, mode: "repulse" }
+    },
+    modes: {
+      repulse: { distance: 100 }
+    }
+  }
+});
